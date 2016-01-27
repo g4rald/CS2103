076cccfb20417acf616b00cc480e67397538e58c
g4rald <gerald_twk@hotmail.com>
2016-01-27 15:54:14 +0800

    Added userCommand()
    Refractor message "command :" + get command from user

--- CityConnect.java
+++ CityConnect.java
@@ -100,12 +100,16 @@ public class CityConnect {
 	public static void main(String[] args) {
 		showToUser(WELCOME_MESSAGE);
 		while (true) {
-			System.out.print("Enter command:");
-			String command = scanner.nextLine();
-			String userCommand = command;
-			String feedback = executeCommand(userCommand);
+			String feedback = executeCommand(userCommand());
 			showToUser(feedback);
 		}
+	}
+
+	private static String userCommand() {
+		System.out.print("Enter command:");
+		String command = scanner.nextLine();
+		String userCommand = command;
+		return userCommand;
 	}
 
 	/*
