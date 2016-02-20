147c1efa30f42116214b42d7e43575b7491d67a8
g4rald <gerald_twk@hotmail.com>
2016-02-21 01:35:40 +0800

    JUnit #2
    - Added about 10+ new test cases
    - Added Before/After

--- CE2/TextBuddyTest.java
+++ CE2/TextBuddyTest.java
@@ -1,12 +1,21 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextBuddyTest {
 
+	@Before 
+	public void initializeFile() throws IOException {
+		TextBuddy.givenFileName = "abc.txt";
+		TextBuddy.clearCommand();
+	}
+	
 	@Test
 	public void testArgumentsEntered() {
 		String[] nullArgument = new String[0];
@@ -22,48 +31,126 @@ public class TextBuddyTest {
 	public void testFileExtension() {
 		assertTrue(TextBuddy.fileExtensionWrong(".lopo"));
 		assertTrue(TextBuddy.fileExtensionWrong(".abc"));
-		assertTrue(TextBuddy.fileExtensionWrong(".xyz"));
-		assertTrue(TextBuddy.fileExtensionWrong("$2adasd"));
-
 		assertFalse(TextBuddy.fileExtensionWrong(".tXt"));
 		assertFalse(TextBuddy.fileExtensionWrong(".TXT"));
-		assertFalse(TextBuddy.fileExtensionWrong(".txt"));
-		assertFalse(TextBuddy.fileExtensionWrong(".TxT"));
 	}
 
 	@Test
 	public void testFileLength() {
 		assertTrue(TextBuddy.insufficentFileLength("txt"));
 		assertTrue(TextBuddy.insufficentFileLength(""));
-		assertTrue(TextBuddy.insufficentFileLength("1$@"));
-		assertTrue(TextBuddy.insufficentFileLength("%@#$"));
-
 		assertFalse(TextBuddy.insufficentFileLength("abc.txt"));
 		assertFalse(TextBuddy.insufficentFileLength("zxcsdasd"));
-		assertFalse(TextBuddy.insufficentFileLength("ab@@s"));
-		assertFalse(TextBuddy.insufficentFileLength("abcd.lol"));
 	}
 
 	@Test
 	public void testInvalidLineNumber() {
 		TextBuddy.totalLinesInFile = 100;
-
-		assertTrue(TextBuddy.invalidLineNumber(-99));
-		assertTrue(TextBuddy.invalidLineNumber(0));
-		assertTrue(TextBuddy.invalidLineNumber(101));
-
-		assertFalse(TextBuddy.invalidLineNumber(100));
-		assertFalse(TextBuddy.invalidLineNumber(50));
-		assertFalse(TextBuddy.invalidLineNumber(1));
+		assertTrue(TextBuddy.isInvalidLineNumber(-99));
+		assertTrue(TextBuddy.isInvalidLineNumber(0));
+		assertFalse(TextBuddy.isInvalidLineNumber(50));
+		assertFalse(TextBuddy.isInvalidLineNumber(1));
+	}
+	
+	@Test
+	public void testAddCommand() throws IOException {
+		String command1 = "add";
+		Scanner sc = new Scanner("stupid dog");
+		String expected = "added to " + TextBuddy.givenFileName + ": \"" + "stupid dog" + "\"";
+		assertEquals(expected,TextBuddy.executeCommand(sc, command1));
+	}
+	
+	@Test
+	public void testClearCommand() throws IOException {
+		String command1 = "clear";
+		Scanner sc = new Scanner(System.in);
+		String expected = "all content deleted from " + TextBuddy.givenFileName;
+		assertEquals(expected,TextBuddy.executeCommand(sc, command1));
+	}
+	
+	@Test
+	public void testDeleteCommand() throws IOException {
+		Scanner sc1 = new Scanner ("haha");
+		Scanner sc2 = new Scanner ("lol");
+		TextBuddy.writeTextToFile(sc1);
+		TextBuddy.writeTextToFile(sc2);
+		String command1 = "delete";
+		Scanner sc3 = new Scanner("2");
+		String expected = "deleted from " + TextBuddy.givenFileName + ": \"" + "lol" + "\"";
+		assertEquals(expected,TextBuddy.executeCommand(sc3, command1));
+	}
+	
+	@Test
+	public void testDisplayCommand() throws IOException {
+		Scanner sc1 = new Scanner ("haha");
+		Scanner sc2 = new Scanner ("lol");
+		TextBuddy.writeTextToFile(sc1);
+		TextBuddy.writeTextToFile(sc2);
+		String command1 = "display";
+		Scanner sc = new Scanner(System.in);
+		String expected = "1. haha\n2. lol";
+		assertEquals(expected,TextBuddy.executeCommand(sc, command1));
+	}
+	
+	@Test
+	public void testSearchCommand() throws IOException {
+		Scanner sc1 = new Scanner ("haha");
+		Scanner sc2 = new Scanner ("lol");
+		TextBuddy.writeTextToFile(sc1);
+		TextBuddy.writeTextToFile(sc2);
+		String command1 = "search";
+		Scanner sc3 = new Scanner("ah");
+		String expected = "Total results : 1";
+		assertEquals(expected,TextBuddy.executeCommand(sc3, command1));
+	}
+	
+	@Test
+	public void testSortCommand() throws IOException {
+		Scanner sc1 = new Scanner ("lol");
+		Scanner sc2 = new Scanner ("haha");
+		TextBuddy.writeTextToFile(sc1);
+		TextBuddy.writeTextToFile(sc2);
+		TextBuddy.sortCommand();
+		Scanner sc = new Scanner(System.in);
+		String command1 = "display";
+		String expected = "1. haha\n2. lol";
+		assertEquals(expected,TextBuddy.executeCommand(sc, command1));
+	}
+	
+	@Test
+	public void testInvalidCommand() throws IOException {
+		Scanner sc = new Scanner(System.in);
+		String command = "zxcas";
+		String expected = "Invalid command! Please retry.";
+		assertEquals(expected,TextBuddy.executeCommand(sc, command));
+	}
+	
+	@Test
+	public void testDisplayEmpty() throws IOException {
+		String command1 = "display";
+		Scanner sc = new Scanner(System.in);
+		String expected = TextBuddy.givenFileName + " is empty";
+		assertEquals(expected,TextBuddy.executeCommand(sc, command1));
+	}
+	
+	@Test
+	public void testSearchNoResults() throws IOException {
+		Scanner sc1 = new Scanner ("haha");
+		Scanner sc2 = new Scanner ("lol");
+		TextBuddy.writeTextToFile(sc1);
+		TextBuddy.writeTextToFile(sc2);
+		String command1 = "search";
+		Scanner sc3 = new Scanner("bbb");
+		String expected = "No such word found!";
+		assertEquals(expected,TextBuddy.executeCommand(sc3, command1));
+	}
+	
+	@After
+	public void deleteFile() throws IOException {
+		File file = new File("abc.txt");
+		TextBuddy.clearCommand();
+		file.delete();
 	}
 
-	@Test
-	public void testTextTrim() throws IOException {
-		Scanner sc = new Scanner("HAHA   \n    LOL\n    ABC     \n     KAPPPA");
-		TextBuddy.givenFileName = "abc.txt";
-		assertEquals("HAHA", TextBuddy.writeTextToFile(sc));
-		assertEquals("LOL", TextBuddy.writeTextToFile(sc));
-		assertEquals("ABC", TextBuddy.writeTextToFile(sc));
-		TextBuddy.clearCommand();
-	}	
+
 }
